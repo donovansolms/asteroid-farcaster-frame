dd0429aef3c0f98b50785f0778690cbb66d15bec
Donovan Solms <4567303+donovansolms@users.noreply.github.com>
2024-02-10 00:07:30 +0200

    Update API endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default async function Home({
 
   const client = new ApolloClient({
     link: new HttpLink({
-      uri: 'http://127.0.0.1:8080/v1/graphql', // Replace this with your GraphQL server URI
+      uri: 'https://api.asteroidprotocol.io/v1/graphql', // Replace this with your GraphQL server URI
     }),
     cache: new InMemoryCache(),
   });
